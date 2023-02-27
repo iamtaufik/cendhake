41bd495fc129c5f363ccf7cedbd05058ce2149cc
muhammadtaufikhidayat <muhtopik07@gmail.com>
2023-02-27 19:37:20 +0700

    menambahkan route handler delete

--- src/pages/api/link/[slug].ts
+++ src/pages/api/link/[slug].ts
@@ -1,10 +1,11 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { prisma } from 'lib/prisma';
+import { getSession } from 'next-auth/react';
 
 export default async function Slug(req: NextApiRequest, res: NextApiResponse) {
+  const { slug } = req.query;
   switch (req.method) {
     case 'GET':
-      const { slug } = req.query;
       try {
         const result = await prisma.link.findUnique({
           where: {
@@ -18,7 +19,21 @@ export default async function Slug(req: NextApiRequest, res: NextApiResponse) {
       } catch (error: any) {
         return res.status(500).json({ message: error.message });
       }
-
+    case 'DELETE':
+      const session = await getSession({ req });
+      if (!session) {
+        return res.status(401).json({ message: 'Tidak terautentikasi' });
+      }
+      try {
+        await prisma.link.delete({
+          where: {
+            slug: String(slug),
+          },
+        });
+        return res.status(200).json({ message: 'Link berhasil dihapus!' });
+      } catch (error: any) {
+        return res.status(500).json({ message: error.message });
+      }
     default:
       return res.status(200).json({ message: 'hello world' });
   }
